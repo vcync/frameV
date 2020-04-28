ac213f7d40a13e62d2b709bb3f4e431b49fddc02
Sam Wray <sam@wray.pro>
2020-04-28 15:56:23 +0100

    updates reload port for prod

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function start() {
   works = JSON.parse(worksJson);
 
   try {
-    reloadInstance = await reload(app, { port: process.env.NODE_ENV === 'production' ? 80 : null });
+    reloadInstance = await reload(app, { port: process.env.NODE_ENV === 'production' ? process.env.PORT : null });
   } catch (err) {
     console.error('Reload could not start, could not start server/sample app', err);
     process.exit();
