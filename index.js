7c393228359b41b79e1b03db38b6daf8131781c8
Sam Wray <sam@wray.pro>
2020-04-28 19:16:41 +0100

    tells reload to use express so http and ws can be on the same port

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function start() {
   works = JSON.parse(worksJson);
 
   try {
-    reloadInstance = await reload(app, { port: process.env.NODE_ENV === 'production' ? parseInt(process.env.PORT, 10) : null });
+    reloadInstance = await reload(app, {}, server);
   } catch (err) {
     console.error('Reload could not start, could not start server/sample app', err);
     process.exit();
